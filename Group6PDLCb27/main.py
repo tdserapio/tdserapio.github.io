037faee5860d7ba3144d76dd8496b52a74ac7478
tdserapio <64088510+tdserapio@users.noreply.github.com>
2022-04-10 11:24:36 +0800

    e

--- Group6PDLCb27/main.py
+++ Group6PDLCb27/main.py
@@ -1,83 +1,67 @@
-import time
-import os
+# Empty Array declaration
+events = []
+joined = []
 
-def pad0(str_n, _len=4):
-    while len(str_n) < _len:
-        str_n += "0"
-    return str_n
+# Variable Initialization/Declaration
+num_events = int(input("Number of events? "))
+print(
+    "\n-------------\nWhen inputting in seconds format:\nThe start, and end of the event in this format without brackets: [start end]. \nFor instance, if the earliest event starts at 6:00PM, then that shall be marked as the 0th second. Then, say for instance 6:10 would be 600 seconds after the first event. Therefore, you shall input that as 600.\n-------------\n"
+)
+counter = 0
 
-def Main():
-    try:
-        print("---- [GWA Calculator] Program starting... ----")
-        time.sleep(1)
-        print("\033[92m---- Program has successfully started ----\033[0m")
-        
-        name = str()
-        name = input("What is your name? ")
-        
-        grades, units = float(), float()
-        numsubj = int()
-        counter = int()
-        
-        counter = 0
-        maxgradevalue = float()
-        maxgradevalue = float("inf")
-        maxgradename = str()
+# Input Loop
+while counter < num_events:
+    # Get name of event
+    name = input("What is the name of this event? ")
 
-        smallestgradevalue = float()
-        smallestgradename = str()
-        
-        numsubj = int(input("How many subjects do you have? "))
-        grade, weight = float(), float()
+    # Processing input of seconds
+    seconds = input("The 'time' of this event in seconds format? ")
+    seconds = seconds.split(" ")
+    curr_event_start = int(seconds[0])
+    curr_event_end = int(seconds[1])
 
-        print(f"\n---- Inputting {name}'s Grades ----\n")
-        
-        while counter < numsubj:
+    # Simply append it to array
 
-            print(f"---- Subject #{counter+1} ----")
-            
-            name = input(f"What is the name of this subject? ")
+    events.append([name, curr_event_start, curr_event_end])
 
-            print("----")
-            
-            grade = float(input("What is your grade in said subject: "))
-            grades += grade
-            
-            weight = float(input("What is the weight (# of units) of said subject: "))
-            units += weight
+    # Counter
+    counter += 1
+    print()
 
-            if grade > smallestgradevalue:
-                smallestgradevalue = grade
-                smallestgradename = name
-                
-            if grade < maxgradevalue:
-                maxgradevalue = grade
-                maxgradename = name
+# Sort the array
 
-            counter += 1
+events = sorted(events, key=lambda x: x[2])
 
-        print(f"\n---- {name}'s Current GWA Status ----")
+# Processing Loop
 
-        print("Your GWA will be: " + pad0(str(grades/units), 5))
-        print("Your highest grade in a subject is: " + str(maxgradename))
-        print("---- With a grade of: " + pad0(str(maxgradevalue)))
-        print("Your lowest grade in a subject is: " + str(smallestgradename))
-        print("---- With a grade of: " + pad0(str(smallestgradevalue)))
-    except Exception as e:
-        print("\033[91m---- ERROR OCCURRED ----")
-        print("This may be due to a wrong input.")
-        print("This is the error: " + str(e).capitalize())
-        print("Shall you try again? [y/n]:")
-        choice = input().lower().strip()
-        if choice == "y":
-            Main()
-        else:
-            print("\033[92m We are very sorry for the inconvinience, have a nice rest of the day!")
-            print("---- Program exiting... ----")
-            time.sleep(0.5)
-            print("---- Program successfully exited ----")
-            return
+while events:
+    # Since array is already sorted, we simply need to get the first element to get the one with the earliest end time.
+    curr_min = events[0]
 
-Main()
+    # This is automatically an event that we shall join.
+    joined.append(curr_min)
 
-print("\033[0m")
+    # Make another array to store the events that are not yet omitted. Although this might cause a Memory problem with very large inputs, I do not have enough brain power to think of something better.
+    # If I do it in the array itself the indexes may overflow and it simply would just be many more weird processing stuff i guess
+    new_events_array = []
+
+    # Loop through the events to see which ones to omit and which ones to not
+    for i in range(len(events)):
+        # Although there is a much more efficient and more flexible and simple method, I decided to just literally check whether the events collide with each other. Besides, I think sets are quite fast.
+        if len(
+                set(range(curr_min[1], curr_min[2])).intersection(
+                    set(range(events[i][1], events[i][2])))) == 0:
+            # If it doesn't well, collide, don't omit it just yet
+            new_events_array.append(events[i])
+    # The new events array is now only filled with non-omitted stuff
+    events = new_events_array
+
+# Output it quite nicely 😌
+print("-------------")
+print("You should join the following events:")
+for i in joined:
+    print(f"\n- Event name: {i[0]}")
+    print(
+        f"  This event shall happen between (in seconds format): {i[1]} until {i[2]}"
+    )
+print("\n")
